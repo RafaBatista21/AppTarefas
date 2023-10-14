7924ca251e45bfe74dc02cae537c1bd7cd441049
Rafael Batista <rafa.rbf@hotmail.com>
2023-10-14 12:22:41 -0300

    Criado projeto

---
+++ src/main/java/AppTarefas/Main.java
@@ -0,0 +1,10 @@
+
+package AppTarefas;
+
+public class Main {
+
+    public static void main(String[] args) {
+ 
+    }
+    
+}
---
+++ src/main/java/model/Task.java
@@ -0,0 +1,16 @@
+
+package model;
+
+import java.util.Date;
+
+public class Task {
+    private int id;
+    private int idProject;
+    private String name;
+    private String description;
+    private String notes;
+    private boolean completed; 
+    private Date deadline;
+    private Date createdAt;
+    private Date updatedAt;
+}
