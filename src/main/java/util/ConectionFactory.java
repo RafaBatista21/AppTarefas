3aa3237e7c7295f77399fda7f98ad96f6a1fb51b
Rafael Batista <rafa.rbf@hotmail.com>
2023-10-15 02:08:14 -0300

    Conectando inicialmente ao banco mysql

---
+++ src/main/java/util/ConectionFactory.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package util;
+
+/**
+ *
+ * @author rafar
+ */
+public class ConectionFactory {
+        
+        private static final String Driver = "com.mysql.jdbc.Driver";
+        private static final String URL = "jdbc:mysql://localhost:3306/apptarefas";
+        private static final String USER = "root";
+        private static final String PASS = "";
+    
+    
+    
+}
