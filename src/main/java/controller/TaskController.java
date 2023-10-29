3149f302b81380c64c4751e38988786f0f021da8
Rafael Batista <rafa.rbf@hotmail.com>
2023-10-29 01:03:20 -0300

    Iniciando a criação dos controladores

---
+++ src/main/java/controller/TaskController.java
@@ -0,0 +1,26 @@
+
+package controller;
+
+/**
+ *
+ * @author rafar
+ */
+public class TaskController {
+    public void save (Task task){
+        
+    }
+    
+    public void update ( Task task){
+        
+    }
+    
+    public void removeById (int TaskId){
+        
+        
+    }
+    public List<Task> getAll ( int idProject){
+        
+    }
+    
+    
+}
