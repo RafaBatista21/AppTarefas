3f8a97a32b871c16916c492716d83a56a5bb24ca
Rafael Batista <rafa.rbf@hotmail.com>
2023-11-05 00:00:16 -0300

    Algumas alteraçoes refeitas

--- src/main/java/controller/TaskController.java
+++ src/main/java/controller/TaskController.java
@@ -5,3 +5,22 @@ package controller;
  *
  * @author rafar
  */
+public class TaskController {
+    public void save (Task task){
+        
+    }
+    
+    public void update ( Task task){
+        
+    }
+    
+    public void removeById (int TaskId){
+        
+        
+    }
+    public List<Task> getAll ( int idProject){
+        
+    }
+    
+    
+}
