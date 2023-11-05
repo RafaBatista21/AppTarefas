700f1ab4fae52cc9948c5b47c130d71ea4ecef9c
Rafael Batista <rafa.rbf@hotmail.com>
2023-11-04 23:58:40 -0300

    Melhorando a versao

--- src/main/java/controller/TaskController.java
+++ src/main/java/controller/TaskController.java
@@ -5,22 +5,3 @@ package controller;
  *
  * @author rafar
  */
-public class TaskController {
-    public void save (Task task){
-        
-    }
-    
-    public void update ( Task task){
-        
-    }
-    
-    public void removeById (int TaskId){
-        
-        
-    }
-    public List<Task> getAll ( int idProject){
-        
-    }
-    
-    
-}
